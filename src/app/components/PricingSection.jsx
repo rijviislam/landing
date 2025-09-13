2c8e86df9c6aafbc74cec71e42ca670ae4bdf24e
rijviislam <rijviislam54@gmail.com>
2025-09-13 21:29:34 +0600

    revert: reverts a previous commit

--- src/app/components/PricingSection.jsx
+++ src/app/components/PricingSection.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import DeleveryIcon from "../icons/DeleveryIcon";
 import RightArrowIcon from "../icons/RightArrowIcon";
 import Button from "./Button";
 import SectionHeader from "./SectionHeader";
@@ -49,7 +50,7 @@ const pricingCard = [
 export default function PricingSection() {
   const [active, setActive] = useState(0);
   return (
-    <section className="bg-background-dark flex items-center justify-center w-full">
+    <section className="bg-background-dark flex flex-col items-center justify-center w-full pb-30">
       <div className="max-w-[1200px] w-full">
         <div className="flex gap-2 flex-col mx-5 md:mx-16 lg:mx-5">
           <SectionHeader
@@ -122,6 +123,37 @@ export default function PricingSection() {
               );
             })}
           </div>
+        </div>
+      </div>
+
+      <div className=" w-full max-w-[1200px] flex flex-col px-5 md:px-16 lg:px-5 xl:px-0">
+        <div className="flex gap-5 my-12 ">
+          <DeleveryIcon />
+          <div className="">
+            <SectionHeader
+              title={"Limited Availability"}
+              className="font-bold text-2xl"
+            />
+            <SectionPtext
+              text={
+                "We only onboard 5 new clients per month. 2 spots left for this month — secure yours today."
+              }
+              className="!text-text-secondary-light"
+            />
+          </div>
+        </div>
+        <div className="bg-card py-12 w-full rounded-2xl flex flex-col items-center ">
+          <div className="flex flex-col gap-2 items-center px-5">
+            <SectionHeader
+              className="text-5xl text-center"
+              title={"Special Offer For Early Clients"}
+            />
+            <SectionPtext
+              text={"Get 10% OFF + Free Hosting Setup when you book this week."}
+              className="text-center"
+            />
+          </div>
+          <Button className="mt-12">Claim my 10% discount</Button>
         </div>
       </div>
     </section>
